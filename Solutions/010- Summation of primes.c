995b6cbad5f580f5b07ee813f594d332b5d60513
João Pedro <83050781+hagsir@users.noreply.github.com>
2022-06-07 01:50:52 -0300

    Update 010- Summation of primes.c

--- Solutions/010- Summation of primes.c
+++ Solutions/010- Summation of primes.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-#include <windows.h>
+#include<windows.h>
 /*
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 int main(){
-long long int i, dividers = 0, n, sum=0, prime;
+long long int i, dividers = 0, n, sum=2;
 
 printf("Calculating the sum of prime numbers up to 2,000,000...");
-    for(n=1; n<2000000; n++){
-        for(i = 1; i <= n; i++){
+    for(n=0; n<2000000; n++){
+    n++;
+        for(i = 0; i <= n; i++){
+        i++;
           if(n % i == 0)
             dividers++;
             if(dividers>2){
@@ -17,11 +19,9 @@ printf("Calculating the sum of prime numbers up to 2,000,000...");
             }
         }
         if(dividers==2){
-                prime;
-                i=prime;
                 sum+=n;
         }
         dividers=0;
     }
-    printf("\nThe sum is %lld.", sum); //The program takes 32 minutes to give the answer
+    printf("\nThe sum is %lld.", sum); //The program takes 14 minutes to give the answer
 }
